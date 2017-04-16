f5c53831ec26b2c07502616e70e33b504e044904
Travis J. Webb <me@traviswebb.com>
2017-04-16 13:27:00 -0400

    [new] create self-referencing module

---
+++ module1.cob
@@ -0,0 +1,11 @@
+       identification division.
+       program-id. module1.
+       environment division.
+       configuration section.
+       repository.
+           function all intrinsic.
+       procedure division.
+       main.
+           display "module-id = " module-id.
+
+
